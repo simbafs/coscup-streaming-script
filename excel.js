14325c4bbbb81e35eca09b0a74cef9ff8c2981f6
SimbaFs <simba.fs@gmail.com>
2024-08-03 22:07:35 +0800

    update

--- excel.js
+++ excel.js
@@ -25,7 +25,7 @@ data.sessions.sort((a, b) => compareString(a.type, b.type))
 
 st = ''
 
-// console.log("日期, 教室, 議程id, 社群id, 社群, 議程名稱, 影片網址")
+console.log("日期, 教室, 議程id, 社群id, 社群, 議程名稱, 影片網址")
 
 const newData = {
     day1: {},
@@ -37,14 +37,14 @@ for (let i = 0; i < data.sessions.length; i++) {
     const s = data.sessions[i]
     const date = new Date(s.start)
 
-    if (date.getDate() !== 29) continue
+    if (date.getDate() !== 3) continue
 
     if (st !== s.type) {
         console.log()
         st = s.type
     }
 
-    // console.log(`${date.getMonth() + 1}/${date.getDate()}, ${s.room}, ${s.id}, ${s.type}, ${sessiontype[s.type]}, "${s.zh.title}"`)
+    console.log(`"${date.getMonth() + 1}/${date.getDate()}", "${s.room}", "${s.id}", "${s.type}", "${sessiontype[s.type]}", "${s.zh.title}"`)
 
     newData.day1[s.room] = [...newData.day1[s.room] || [], s]
 }
@@ -54,15 +54,15 @@ for (let i = 0; i < data.sessions.length; i++) {
     const s = data.sessions[i]
     const date = new Date(s.start)
 
-    if (date.getDate() !== 30) continue
+    if (date.getDate() !== 4) continue
 
     if (st !== s.type) {
         console.log()
         st = s.type
     }
 
-    // console.log(`${date.getMonth() + 1}/${date.getDate()}, ${s.room}, ${s.id}, ${s.type}, ${sessiontype[s.type]}, "${s.zh.title}"`)
+    console.log(`"${date.getMonth() + 1}/${date.getDate()}", "${s.room}", "${s.id}", "${s.type}", "${sessiontype[s.type]}", "${s.zh.title}"`)
     newData.day2[s.room] = [...newData.day2[s.room] || [], s]
 }
 
-console.log(JSON.stringify(newData, null, 2))
+// console.log(JSON.stringify(newData, null, 2))
